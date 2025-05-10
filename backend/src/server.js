dd1a30a9242541fc697419c70407529808019148
Siddharth <siddharth20630901@gmail.com>
2025-05-10 20:54:15 +0545

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,10 @@ import authRoutes from "./routes/auth.route.js"
 import  userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js";
-
+import path from "path";
 const app = express();
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 
 app.use(cors(
     {
@@ -24,7 +24,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 app.listen(PORT,()=>{
