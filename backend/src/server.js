3a290d218119d7cfd1401d333b5ddd11e0abe5fb
Siddharth <siddharth20630901@gmail.com>
2025-05-10 20:30:41 +0545

    Describe the changes you've made

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 
 app.use(cors(
     {
-        origin:"https://chat-web-app-1-baqy.onrender.com",
+        origin:"http://localhost:5173",
         credentials:true
     }
 ))
